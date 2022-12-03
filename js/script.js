b635ffced06654c3032bfb7c73eb5b7a0d5ba7d1
Caleus963(AbertoFsS) <1835080alberto@gmail.com>
2022-12-03 13:18:24 -0600

    primera version de una pagina con divs

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let boton1= document.querySelector("#prueba1");
+
+let boton2= document.querySelector("#prueba2");
+
+boton1.addEventListener("click", function(evento){
+    console.log("A")
+})
+
+boton2.addEventListener("click", function(evento){
+    console.log("B")
+})
